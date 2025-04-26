05f3b81082a9e08617b38e1a358c1fbf5c8c3ed8
mahmoudalkhrsa <mahmoudalkh831@gmail.com>
2025-04-26 10:19:45 +0300

    Add files via upload

---
+++ Divisibility Problem/Divisibility Problem/Divisibility Problem.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<vector>	
+using namespace std;
+int NumberOfStepsToMake_A_DivisibleBy_B_(int a, int b)
+{
+	if (a % b == 0)
+		return 0;
+	else
+		return b - (a % b);
+}
+int main()
+{
+	int t=0,d=0;
+	cin >> t;
+	vector<int>ste;
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+		ste.push_back(NumberOfStepsToMake_A_DivisibleBy_B_(a, b));
+		d++;
+	}
+	for (int &c : ste)
+	{
+		cout << c << endl;
+	}
+}
